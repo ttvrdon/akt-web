734f64c97b9bed0ea34b74fc4db95ea0c3685a8a
Tomas Tvrdon <tomas.tvrdon@resideo.com>
2026-02-24 00:42:36 +0100

    keepAlive removed for test

--- api/Functions/KeepAlive.cs
+++ api/Functions/KeepAlive.cs
@@ -1,18 +1,18 @@
-﻿using Microsoft.Azure.Functions.Worker;
-using Microsoft.Azure.Functions.Worker.Http;
-using System.Net;
+﻿//using Microsoft.Azure.Functions.Worker;
+//using Microsoft.Azure.Functions.Worker.Http;
+//using System.Net;
 
-namespace AktWeb.Functions.Functions;
+//namespace AktWeb.Functions.Functions;
 
-public class KeepAlive
-{
-    [Function("keepAlive")]
-    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
-    {
-        var response = req.CreateResponse(HttpStatusCode.OK);
-        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-        response.WriteString("I'm awake!");
+//public class KeepAlive
+//{
+//    [Function("keepAlive")]
+//    public static HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+//    {
+//        var response = req.CreateResponse(HttpStatusCode.OK);
+//        response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+//        response.WriteString("I'm awake!");
 
-        return response;
-    }
-}
+//        return response;
+//    }
+//}
